062e9040e711cc34c5e869a368d5984d7c92127e
Sudara <sudara@alonetone.com>
2022-01-01 15:48:46 +0100

    Update to Catch2 v3

--- Tests/PluginBasics.cpp
+++ Tests/PluginBasics.cpp
@@ -1,13 +1,16 @@
-#include "catch2/catch.hpp"
 #include <PluginProcessor.h>
+#include <catch2/catch_all.hpp>
 
 TEST_CASE("one is equal to one", "[dummy]")
 {
-	REQUIRE(1 == 1);
+  REQUIRE(1 == 1);
 }
 
 // https://github.com/McMartin/FRUT/issues/490#issuecomment-663544272
 AudioPluginAudioProcessor testPlugin;
-TEST_CASE("Plugin instance name", "[name]") {
-    CHECK_THAT( testPlugin.getName().toStdString(), Catch::Equals( "Pamplejuce" ) );
+
+TEST_CASE("Plugin instance name", "[name]")
+{
+  CHECK_THAT(testPlugin.getName().toStdString(),
+             Catch::Matchers::Equals("Pamplejuce"));
 }
