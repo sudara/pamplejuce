945b90dd30a2ae535d7952e206490398f256deb6
Sudara <sudara@alonetone.com>
2023-09-04 14:13:00 +0200

    I was lied to, CMAKE_CURRENT_LIST_DIR isn't always a list file!

--- cmake/GitHubENV.cmake
+++ cmake/GitHubENV.cmake
@@ -1,7 +1,7 @@
 # Write some temp files to make GitHub Actions / packaging easy
 
 if ((DEFINED ENV{CI}))
-    set (env_file "${CMAKE_CURRENT_LIST_DIR}/.pamplejuce_env")
+    set (env_file "${PROJECT_SOURCE_DIR}/.pamplejuce_env")
     message ("Writing ENV file for CI: ${env_file}")
     file(APPEND env_file "${PROJECT_NAME}\\n")
     file(APPEND env_file "${PRODUCT_NAME}\\n")
