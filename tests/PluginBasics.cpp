7c605cd35481804e7e7a811a540c467043a59816
Sudara <sudara@alonetone.com>
2023-09-04 15:10:16 +0200

    Fix test depending on plugin name

--- tests/PluginBasics.cpp
+++ tests/PluginBasics.cpp
@@ -13,7 +13,7 @@ PluginProcessor testPlugin;
 TEST_CASE("Plugin instance name", "[name]")
 {
   CHECK_THAT(testPlugin.getName().toStdString(),
-             Catch::Matchers::Equals("Pamplejuce"));
+             Catch::Matchers::Equals("Pamplejuce Demo"));
 }
 
 #ifdef PAMPLEJUCE_IPP
