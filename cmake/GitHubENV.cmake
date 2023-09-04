c24d70072eea5e72447750125a4e98bd611fac50
Sudara <sudara@alonetone.com>
2023-09-04 14:29:42 +0200

    Debug .env

--- cmake/GitHubENV.cmake
+++ cmake/GitHubENV.cmake
@@ -3,7 +3,9 @@
 if ((DEFINED ENV{CI}))
     set (env_file "${PROJECT_SOURCE_DIR}/.env")
     message ("Writing ENV file for CI: ${env_file}")
-    file(APPEND env_file "${PROJECT_NAME}\\n")
+
+    # the first call truncates, the rest append
+    file(WRITE  env_file "${PROJECT_NAME}\\n")
     file(APPEND env_file "${PRODUCT_NAME}\\n")
     file(APPEND env_file "${BUNDLE_ID}\\n")
     file(APPEND env_file "${COMPANY_NAME}\\n")
