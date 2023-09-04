d647deadca30b616ba8eacef62708a5ee2c3390e
Sudara <sudara@alonetone.com>
2023-09-04 13:40:42 +0200

    Stick the .env at the root of the project

--- cmake/GitHubENV.cmake
+++ cmake/GitHubENV.cmake
@@ -2,7 +2,7 @@
 
 if (ENV{CI} STREQUAL 1)
     message ("Writing ENV file for CI")
-    set (env_file "${CMAKE_CURRENT_BINARY_DIR}/.pamplejuce_env")
+    set (env_file "${CMAKE_CURRENT_LIST_DIR}/.pamplejuce_env")
     file(APPEND env_file "${PROJECT_NAME}\\n")
     file(APPEND env_file "${PRODUCT_NAME}\\n")
     file(APPEND env_file "${BUNDLE_ID}\\n")
