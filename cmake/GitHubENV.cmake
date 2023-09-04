dc4b70c31441ddbd364cc670c3e93a4c92b2aa17
Sudara <sudara@alonetone.com>
2023-09-04 14:50:00 +0200

    env_file needs quotes

--- cmake/GitHubENV.cmake
+++ cmake/GitHubENV.cmake
@@ -5,8 +5,8 @@ if ((DEFINED ENV{CI}))
     message ("Writing ENV file for CI: ${env_file}")
 
     # the first call truncates, the rest append
-    file(WRITE  env_file "${PROJECT_NAME}\\n")
-    file(APPEND env_file "${PRODUCT_NAME}\\n")
-    file(APPEND env_file "${BUNDLE_ID}\\n")
-    file(APPEND env_file "${COMPANY_NAME}\\n")
+    file(WRITE  "${env_file}" "${PROJECT_NAME}\\n")
+    file(APPEND "${env_file}" "${PRODUCT_NAME}\\n")
+    file(APPEND "${env_file}" "${BUNDLE_ID}\\n")
+    file(APPEND "${env_file}" "${COMPANY_NAME}\\n")
 endif ()
