9724c8ef8691f078667f92404a34c579fec6634b
Sudara <sudara@alonetone.com>
2023-09-04 17:08:31 +0200

    Add rest of PROJECT/PRODUCT disambiguation (Thx @The-Him)

--- cmake/GitHubENV.cmake
+++ cmake/GitHubENV.cmake
@@ -7,6 +7,7 @@ if ((DEFINED ENV{CI}))
     # the first call truncates, the rest append
     file(WRITE  "${env_file}" "PROJECT_NAME=${PROJECT_NAME}\n")
     file(APPEND "${env_file}" "PRODUCT_NAME=${PRODUCT_NAME}\n")
+    file(APPEND "${env_file}" "VERSION=${CURRENT_VERSION}\n")
     file(APPEND "${env_file}" "BUNDLE_ID=${BUNDLE_ID}\n")
     file(APPEND "${env_file}" "COMPANY_NAME=${COMPANY_NAME}\n")
 endif ()
