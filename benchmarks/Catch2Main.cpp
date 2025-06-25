d6a5662fd418800c177c698889b8de1537c8c52e
Sudara <sudara@alonetone.com>
2025-06-25 14:51:22 +0200

    Bump dependencies.

--- benchmarks/Catch2Main.cpp
+++ benchmarks/Catch2Main.cpp
@@ -15,3 +15,9 @@ int main (int argc, char* argv[])
 
     return result;
 }
+
+#include "PluginEditor.h"
+#include "catch2/benchmark/catch_benchmark_all.hpp"
+#include "catch2/catch_test_macros.hpp"
+
+#include "Benchmarks.cpp"
